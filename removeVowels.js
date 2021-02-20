25690d50fb5f2bf5ff0813d02d940ec7fb05f6c7
barry.mulrooney <barry.mulrooney@mulrooneydesign.ie>
2021-02-20 17:40:18 +0100

    Renamed variables and removed unnecessary comments.

--- removeVowels.js
+++ removeVowels.js
@@ -1,19 +1,18 @@
 function removeVowels(string) {
   
-   //Turn string into a array
-  let stringArray = Array.from(string);
+  let stringToArray = Array.from(string)
 
-  //Function to check is string contains vowels
-  let stringArrayNoVowels =  stringArray.filter(function(e) {
+  let stringArrayNoVowels =  stringToArray.filter(function(e) {
    
     if(e.toLowerCase() != "a" && e.toLowerCase() != "e" && e.toLowerCase() != "i"  && e.toLowerCase() != "o"  && e.toLowerCase() != "u") {
-      return e;
+      return e
     }
-});
+})
   
-  let finalArray = stringArrayNoVowels.join("");  
-  console.log(finalArray);
-  return finalArray;
+  let stringNoVowels = stringArrayNoVowels.join("")
+  console.log(stringNoVowels)
+  return stringNoVowels
 }
 
-removeVowels("Hello there people. This is a sentence without Vowels.");
+//Example:
+removeVowels("Hello there people. This is a sentence without Vowels.")
